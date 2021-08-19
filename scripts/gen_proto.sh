30ac0a4d5a97037b07828119c26f98fa7bf263a1
Billy Pham <visopsys@users.noreply.github.com>
2021-08-18 23:41:52 -0700

    Keygen proto

--- scripts/gen_proto.sh
+++ scripts/gen_proto.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# protoc proto/common/partyid.proto proto/ecdsa/keygen.proto -I. --go_out=types
-
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 CUR_DIR=$(pwd)
 CUR_DIR_LENGTH=${#CUR_DIR}
-echo $CUR_DIR_LENGTH
 
+# Gets all files in the proto folder and save them into an array.
 arr=()
-
 for folder in $CUR_DIR/proto/**
 do
   relative_path=${folder:$CUR_DIR_LENGTH+1}
@@ -18,13 +15,11 @@ do
   do
     f=$relative_path/$(basename $file)
     arr+=($f)
-    # protoc $file -I. --go_out=types
   done
 done
 
 # Join all relative path
 s=$(join_by " " "${arr[@]}")
-echo $s
 
 # Generate all proto files. These will go into folder github.com/sisu-network/dheart
 protoc $s -I. --go_out=.
